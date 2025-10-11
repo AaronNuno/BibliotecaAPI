71b0df926b8103d2f06d45de9a7e6473dd154f13
Aaron Nuno <aaron_a_7@hotmail.com>
2025-10-10 21:48:07 -0600

    add controller and entity

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,23 @@
+﻿using BibliotecaAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+
+        public IEnumerable<Autor> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Felipe" },
+                new Autor() { Id = 2, Nombre = "Claudia" },
+                new Autor() { Id = 3, Nombre = "Autor 3" }
+            };
+        }
+    }
+}
