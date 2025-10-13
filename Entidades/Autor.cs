e0f606f829cfbe9b09ea94d2eaf812d467aae0e5
Aaron Nuno <aaron_a_7@hotmail.com>
2025-10-13 17:57:22 -0600

    add Valitations

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -5,8 +5,17 @@ namespace BibliotecaAPI.Entidades
     public class Autor
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="The field {0} is require")]
+        [StringLength(150,ErrorMessage = "The field {0} must not be longer than {1} characters or less")]
         public required string Nombre { get; set; }
         public List<Libro> Libros { get; set; } = new List<Libro>();
+
+        /*[Range(18, 120)]
+        public int Edad {  get; set; }
+        [CreditCard]
+        public string? TarjetaCredito { get; set; }
+         [Url]
+        public string? URL   { get; set; }
+       */
     }
 }
