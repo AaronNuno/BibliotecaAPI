e2ee6e5c07f1e3489cab3084835066e106ba87da
Aaron Nuno <aaron_a_7@hotmail.com>
2025-10-15 22:20:25 -0600

    add middlewares

--- Program.cs
+++ Program.cs
@@ -19,6 +19,36 @@ var app = builder.Build();
 
 // add middleware 
 
+
+// Log
+app.Use(async (contexto, next) =>
+{
+    // Viene petición
+    var logger = contexto.RequestServices.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation($"Peitición: {contexto.Request.Method} {contexto.Request.Path}");
+
+    await next.Invoke();
+    
+    // Se va la petición
+
+    logger.LogInformation($"Respuesta: {contexto.Response.StatusCode}");
+});
+
+app.Use(async (contexto, next) =>
+{
+    if(contexto.Request.Path == "/bloqueado")
+    {
+        contexto.Response.StatusCode = 403;
+        await contexto.Response.WriteAsync("Acceso denegado");
+    }
+    else
+    {
+        await next.Invoke();
+    }
+
+});
+
+
 app.MapControllers();
 
 app.Run();
