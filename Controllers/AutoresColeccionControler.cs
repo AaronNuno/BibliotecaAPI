d6bb2cfb95c5898e37d7af87163f1bd38d6dcc68
Aaron Nuno <aaron_a_7@hotmail.com>
2025-10-27 20:00:18 -0600

    Creando una coleccion de recursos

---
+++ Controllers/AutoresColeccionControler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BibliotecaAPI.Datos;
+using BibliotecaAPI.DTOs;
+using BibliotecaAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores-coleccion")]
+    public class AutoresColeccionControler: ControllerBase
+    {
+        private readonly AplicationDBContext context;
+        private readonly IMapper mapper;
+
+        public AutoresColeccionControler(AplicationDBContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(IEnumerable<AutorCreacionDTO> autoresCreacionDTO)
+        {
+            var autores = mapper.Map<IEnumerable<Autor>>(autoresCreacionDTO);
+            context.AddRange(autores);
+            await context.SaveChangesAsync();
+            return Ok();
+            
+        }
+
+
+    }
+}
