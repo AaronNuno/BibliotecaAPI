3ab9a80f4cb4f1445d2979c53f581672585773f4
Aaron Nuno <aaron_a_7@hotmail.com>
2025-10-27 20:28:48 -0600

    Obteniendo una Colección de Recursos

--- Controllers/AutoresColeccionControler.cs
+++ Controllers/AutoresColeccionControler.cs
@@ -3,6 +3,8 @@ using BibliotecaAPI.Datos;
 using BibliotecaAPI.DTOs;
 using BibliotecaAPI.Entidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace BibliotecaAPI.Controllers
 {
@@ -18,6 +20,39 @@ namespace BibliotecaAPI.Controllers
             this.context = context;
             this.mapper = mapper;
         }
+        [HttpGet("{ids}", Name = "ObetenerAutoresPorIds")]
+        public async Task<ActionResult<List<AutorConLibrosDTO>>> Get(string ids)
+        {
+            var idsColeccion = new List<int>();
+
+            foreach (var id in ids.Split(","))
+            {
+                if (int.TryParse(id, out int idInt))
+                {
+                    idsColeccion.Add(idInt);
+
+                }
+            }
+
+            if (!idsColeccion.Any())
+            {
+                ModelState.AddModelError(nameof(ids), "Ningun Id fue encontrado");
+                return ValidationProblem();
+            }
+
+            var autores = await context.Autores
+                .Include(x => x.Libros)
+                .ThenInclude(x => x.Libro)
+                .Where(x => idsColeccion.Contains(x.Id))
+                .ToListAsync();
+
+            if (autores.Count != idsColeccion.Count)
+            {
+                return NotFound();
+            }
+            var autoresDTO = mapper.Map<List<AutorConLibrosDTO>>(autores);
+            return autoresDTO;
+        }
 
         [HttpPost]
         public async Task<ActionResult> Post(IEnumerable<AutorCreacionDTO> autoresCreacionDTO)
@@ -25,7 +60,12 @@ namespace BibliotecaAPI.Controllers
             var autores = mapper.Map<IEnumerable<Autor>>(autoresCreacionDTO);
             context.AddRange(autores);
             await context.SaveChangesAsync();
-            return Ok();
+
+            var autoresDTO = mapper.Map<IEnumerable<AutorDTO>>(autores);
+            var ids = autores.Select(x => x.Id);
+            var idsString = string.Join(",", ids);
+
+            return CreatedAtRoute("ObetenerAutoresPorIds", new { ids = idsString }, autoresDTO);
             
         }
 
