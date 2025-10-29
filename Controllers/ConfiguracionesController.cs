76374989ea4ed655f76dfe2a3c743b5f3dd2e4db
Aaron Nuno <aaron_a_7@hotmail.com>
2025-10-28 18:47:45 -0600

    Ejemplo de configuraciones , secciones

---
+++ Controllers/ConfiguracionesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/configuracion")]
+    public class ConfiguracionesController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ConfiguracionesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        [HttpGet]
+        public ActionResult<string> Get() 
+            {
+            var opcion1 = configuration["apellido"];
+
+            var opcion2 = configuration.GetValue<string>("apellido")!;
+
+            return opcion2;
+
+            }
+
+        [HttpGet("secciones")]
+        public ActionResult<string>GetSeccion()
+        {
+            var opcion1 = configuration["ConnectionStrings:DefaultConnection"];
+
+            var opcion2 = configuration.GetValue<string>("ConnectionStrings");
+
+            var seccion = configuration.GetSection("ConnectionStrings");
+
+            var opcion3 = seccion["DefaultConnection"];
+
+            return opcion3!;
+        }
+
+
+
+    }
+}
