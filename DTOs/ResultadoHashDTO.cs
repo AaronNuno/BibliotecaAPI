a1784b1701b5235b32b3cf5f1ae238d810b4f1c1
Aaron Nuno <aaron_a_7@hotmail.com>
2025-11-05 22:20:50 -0600

    Ejemplo hash ASP.NET Core

---
+++ DTOs/ResultadoHashDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaAPI.DTOs
+{
+    public class ResultadoHashDTO
+    {
+        public required  string hash { get; set; }
+        public required byte[] Sal {  get; set; }   
+
+    }
+}
